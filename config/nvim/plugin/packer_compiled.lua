5daaa62929e2d35d4421ff6440c2134a80445b16
Zach Brady <3282673+zzzbra@users.noreply.github.com>
2023-02-22 23:28:41 -0500

    Make updates based on Ubuntu set up

--- config/nvim/plugin/packer_compiled.lua
+++ config/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/zzzbra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,104 +76,104 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["Comment.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/Comment.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
   },
   LuaSnip = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["fidget.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/fidget.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/fidget.nvim",
     url = "https://github.com/j-hui/fidget.nvim"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   gruvbox = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/gruvbox",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/gruvbox",
     url = "https://github.com/morhetz/gruvbox"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["kanagawa.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
     url = "https://github.com/rebelot/kanagawa.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["neodev.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/neodev.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/neodev.nvim",
     url = "https://github.com/folke/neodev.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-treesitter-textobjects"] = {
     load_after = {},
     loaded = true,
     needs_bufread = false,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/opt/nvim-treesitter-textobjects",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/opt/nvim-treesitter-textobjects",
     url = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects"
   },
   ["onedark.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/onedark.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/onedark.nvim",
     url = "https://github.com/navarasu/onedark.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope-fzf-native.nvim"] = {
@@ -181,27 +181,27 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/opt/telescope-fzf-native.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/opt/telescope-fzf-native.nvim",
     url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-rhubarb"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/vim-rhubarb",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/vim-rhubarb",
     url = "https://github.com/tpope/vim-rhubarb"
   },
   ["vim-sleuth"] = {
     loaded = true,
-    path = "/Users/zzzbra/.local/share/nvim/site/pack/packer/start/vim-sleuth",
+    path = "/home/zzzbra/.local/share/nvim/site/pack/packer/start/vim-sleuth",
     url = "https://github.com/tpope/vim-sleuth"
   }
 }
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,15 +6,19 @@
 
 ########## Variables
 
-dir=~/dotfiles                              # dotfiles directory
-olddir=~/dotfiles_old                       # old dotfiles backup directory
-files="zshrc tmux.conf config/nvim"         # list of files/folders to symlink in homedir
+# dotfiles directory
+# TODO: allow for this to live in dev dir
+dir=~/dotfiles                              
+# old dotfiles backup directory; use timestamp so we don't overwrite second oldest
+old_dir=~/dotfiles/backups/$(date +%s)                       
+# list of files/folders to symlink in homedir
+files="zshrc tmux.conf config/nvim gitconfig gitignore"         
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+echo "Creating $old_dir for backup of any existing dotfiles in ~"
+mkdir -p $old_dir
 echo "...done"
 
 # change to the dotfiles directory
@@ -23,9 +27,13 @@ cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+echo "Moving any existing dotfiles from ~ to $old_dir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    if [ -n ~/.$file ]; then
+        echo "Found any existing ~/.$file; moving to ~ to $old_dir"
+        mv ~/.$file "$old_dir/" 
+    fi
+
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
